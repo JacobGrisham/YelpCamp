963224f534e80392c44e2ab4ee70c4b876a37dad
Jacob Grisham <jacob.d.grisham@gmail.com>
2020-08-24 23:51:34 +0000

    Only show loader for home and landing page

--- public/index.js
+++ public/index.js
@@ -1,10 +1,10 @@
 // Loading screen
 document.onreadystatechange = () => { 
   if (document.readyState !== "complete") { 
-      document.querySelector("main").style.visibility = "hidden"; 
+      document.querySelector(".landing-page").style.visibility = "hidden"; 
       document.querySelector(".loader").style.visibility = "visible"; 
   } else { 
       document.querySelector(".loader").style.display = "none"; 
-      document.querySelector("main").style.visibility = "visible"; 
+      document.querySelector(".landing-page").style.visibility = "visible"; 
   } 
 }; 
