24b895c1ca8493bf1567cbb28a106e4990b71481
Jacob Grisham <jacob.d.grisham@gmail.com>
2021-12-02 22:41:52 -0800

    remove application code logic from analytics javascript file

--- public/stylesheets/analytics.js
+++ public/stylesheets/analytics.js
@@ -3,31 +3,3 @@ window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
   gtag('config', 'UA-139163603-2');
-
-// Toggle Daytime and Nighttime
-const btn = document.querySelector('.btn-toggle');
-
-btn.addEventListener('click', function() {
-  document.body.classList.toggle('nighttime');  
-})
-
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-(function () {
-  'use strict'
-
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  var forms = document.querySelectorAll('.needs-validation')
-
-  // Loop over them and prevent submission
-  Array.prototype.slice.call(forms)
-    .forEach(function (form) {
-      form.addEventListener('submit', function (event) {
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-        }
-
-        form.classList.add('was-validated')
-      }, false)
-    })
-})()
